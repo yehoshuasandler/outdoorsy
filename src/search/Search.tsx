bc79fe133120d84ecc4951b9d62fd310bd460db1
Joshua Shoemaker <joshuashoemaker@outlook.com>
2023-08-10 16:38:43 -0500

    style: whitespaceing

--- src/search/Search.tsx
+++ src/search/Search.tsx
@@ -24,27 +24,27 @@ export const Search = () => {
     const keysMimickingSubmit = [" ", ","]
 
     if (keysMimickingSubmit.includes(e.key)) {
-     formRef.current?.requestSubmit()
+      formRef.current?.requestSubmit()
     }
   }
-  
+
 
   return <form ref={formRef} onSubmit={handleSubmit} className='relative'>
     <label
-        htmlFor="search"
-        className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-      >
-        Search
-      </label>
-      <input
-        type="text"
-        name="search"
-        id="search"
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        placeholder="Relaxing, sun soaked, etc."
-        onKeyDown={handleKeyDown}
-        ref={keywordRef}
-      />
+      htmlFor="search"
+      className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+    >
+      Search
+    </label>
+    <input
+      type="text"
+      name="search"
+      id="search"
+      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      placeholder="Relaxing, sun soaked, etc."
+      onKeyDown={handleKeyDown}
+      ref={keywordRef}
+    />
     <Keywords keywords={search.filterKeywords} />
   </form>
 }
